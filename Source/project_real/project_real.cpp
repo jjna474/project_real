dac5e8055c77025a816b9e014d03c8876f495895
jina555 <kimjina0309@naver.com>
2026-05-21 14:42:36 +0900

    Initial commit

---
+++ Source/project_real/project_real.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "project_real.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, project_real, "project_real" );
